73a9d3f0019ab6499526e73dc2adeef89d64c2e0
Christian Ranz <git@christianranz.com>
2011-04-09 11:44:15 +0000

    some error handling inside the toFile Module

--- modules/toFile.js
+++ modules/toFile.js
@@ -17,58 +17,66 @@ var fs = require('fs'),
 
 // the post request handler
 exports.onpost = function(req, res){
-    var microtime = new Date().getTime(),
-        dateUTC = new Date().toUTCString();
-    var fileName = config.fileName;
-    fileName = fileName.replace(/%microtime%/g, microtime);
-    fileName = fileName.replace(/%dateUTC%/g, dateUTC);
-    fileName = fileName.replace(/ /g, '_');
-    fileName = fileName.replace(/\//g, '');
-    fileName = fileName.replace(/,/g, '');
-    fileName = fileName.replace(/:/g, '-');
-    
-    // init output stream
-    var outputStream = fs.createWriteStream(
-        config.outputPath+'/'+fileName,
-        { 
-            flags: 'w',
-            encoding: null,
-            mode: 0666 
-        }
-    );
-    
-    // pump data from request to output
-    util.pump(req, outputStream, function(error){
-        // on outputStream error
-    });
-    
-    // no more data
-    req.on('end', function(){
-        outputStream.end();
+    var reqUrl = url.parse(req.url, true);
+    if(reqUrl.pathname === '/results' || reqUrl.pathname === '/results/'){
+        var microtime = new Date().getTime(),
+            dateUTC = new Date().toUTCString();
+        var fileName = config.fileName;
+        fileName = fileName.replace(/%microtime%/g, microtime);
+        fileName = fileName.replace(/%dateUTC%/g, dateUTC);
+        fileName = fileName.replace(/ /g, '_');
+        fileName = fileName.replace(/\//g, '');
+        fileName = fileName.replace(/,/g, '');
+        fileName = fileName.replace(/:/g, '-');
         
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        // build response object
-        var responseJson = {
-            status: 200,
-            error: false,
-            message: 'OK',
-            action: 'post',
-            generatedfile: fileName
-        };
+        // init output stream
+        var outputStream = fs.createWriteStream(
+            config.outputPath+'/'+fileName,
+            { 
+                flags: 'w',
+                encoding: null,
+                mode: 0666 
+            }
+        );
         
-        // send response data
-        res.write(JSON.stringify(responseJson));
-        // send signal module finished
+        // pump data from request to output
+        util.pump(req, outputStream, function(error){
+            res.writeHead(500);
+            res.end();
+            return;
+        });
+        
+        // no more data
+        req.on('end', function(){
+            outputStream.end();
+            
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            // build response object
+            var responseJson = {
+                status: 200,
+                error: false,
+                message: 'OK',
+                action: 'post',
+                generatedfile: fileName
+            };
+            
+            // send response data
+            res.write(JSON.stringify(responseJson));
+            // send signal module finished
+            res.end();
+        });
+    }
+    else {
         res.end();
-    });
+    }
 };
 
 exports.onget = function(req, res){
     var reqUrl = url.parse(req.url, true);
     if((reqUrl.pathname === '/results/' || reqUrl.pathname === '/results') && reqUrl.query.file === 'list'){
         fs.readdir(config.outputPath, function(err, files){
-            res.writeHead(200, {'Content-Type': 'application/json'});
             if(!err){
+                res.writeHead(200, {'Content-Type': 'application/json'});
                 var responseJson = {
                     status: 200,
                     error: false,
@@ -78,6 +86,7 @@ exports.onget = function(req, res){
                 };
             }
             else{
+                res.writeHead(500, {'Content-Type': 'application/json'})
                 var responseJson = {
                     status: 500,
                     error: true,
@@ -91,10 +100,21 @@ exports.onget = function(req, res){
         });
     }
     else if((reqUrl.pathname === '/results/' || reqUrl.pathname === '/results') && reqUrl.query.file !== 'list'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
+        fs.stat(config.outputPath+'/'+reqUrl.query.file, function(err, stat){
+            if(err){
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+        });
         var readStream = fs.createReadStream(config.outputPath+'/'+reqUrl.query.file);
-        util.pump(readStream, res);
+        util.pump(readStream, res, function(){
+            res.writeHead(500);    
+            res.end();
+            return;
+        });
         readStream.on('end', function(){
+            res.writeHead(200, {'Content-Type': 'application/json'});
             res.end();
         });
     }
