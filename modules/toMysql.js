82399dab9d8141fd780a5e696afe7aa109522b4d
Christian Ranz <git@christianranz.com>
2011-04-20 09:52:21 +0000

    typo

--- modules/toMysql.js
+++ modules/toMysql.js
@@ -60,7 +60,7 @@ exports.onpost = function(req, res){
                 
             var userAgent = info['window.navigator.userAgent'];
             db.query("INSERT into "+config.TABLE_NAME+" (info, tests, useragent) VALUES ('"+infoRaw+"', '"+testsRaw+"', '"+userAgent+"');",function(err,results,fields){
-                console.log(error);
+                console.log(err);
             });
         });
     }
