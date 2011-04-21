14ba74ae9236cde5863ad4d41d48b653e80d4879
Christian Ranz <git@christianranz.com>
2011-04-21 10:34:54 +0000

    testing stuff

--- modules/toMongo.js
+++ modules/toMongo.js
@@ -117,6 +117,7 @@ exports.onget = function(req,res){
     if((reqUrl.pathname === '/results/' || reqUrl.pathname === '/results') && reqUrl.query.result === 'list'){
         db.open(function(error, db){
             if(error){
+                console.log('db');
                 console.log(error.message);
                 db.close();
                 res.end();
@@ -125,15 +126,16 @@ exports.onget = function(req,res){
             else{
                 db.collection(config.collection, function(error, collection){
                     if(error){
+                        console.log('collection');
                         console.log(error.message);
                         db.close();
                         res.end();
                         return;
                     }
                     else{
-                        var list = collection.find({}, {'_id': 1});
+                        //var list = collection.find({}, {'_id': 1});
                         res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
-                        res.write(JSON.stringify(list));
+                        //res.write(JSON.stringify(list));
                         res.end();
                         db.close();
                     }
